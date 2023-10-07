bb2f655372896a308e8aae40fe195a6074f465c4
homosapien14 <spma259@gmail.com>
2023-10-07 17:36:30 +0530

    minor changes

--- src/components/molecules/buttonGroup/buttonGroup.molecule.js
+++ src/components/molecules/buttonGroup/buttonGroup.molecule.js
@@ -3,11 +3,7 @@ import Button from "../button/button.molecule";
 
 const ButtonGroup = ({ buttons, selected, setSelected, numberOfGrid }) => {
   return (
-    <div
-      className={`grid grid-cols-1 sm:grid-cols-${
-        numberOfGrid ? numberOfGrid : 4
-      } `}
-    >
+    <div className={`grid grid-cols-1 sm:grid-cols-4 `}>
       {buttons.map((button, index) => (
         <Button
           key={index}
